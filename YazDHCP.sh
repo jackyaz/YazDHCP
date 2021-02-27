11db572dd6c6e3bba44efdab0bf999989006ad15
Jack Naisbett <jack.naisbett@gmail.com>
2021-02-27 19:38:13 +0000

    Version bump

--- YazDHCP.sh
+++ YazDHCP.sh
@@ -15,7 +15,7 @@
 
 ### Start of script variables ###
 readonly SCRIPT_NAME="YazDHCP"
-readonly SCRIPT_VERSION="v1.0.1"
+readonly SCRIPT_VERSION="v1.0.2"
 SCRIPT_BRANCH="master"
 SCRIPT_REPO="https://raw.githubusercontent.com/jackyaz/$SCRIPT_NAME/$SCRIPT_BRANCH"
 readonly SCRIPT_DIR="/jffs/addons/$SCRIPT_NAME.d"
